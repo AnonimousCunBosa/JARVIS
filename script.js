779d190d7a2e49f3356b115312ad8b5e3347c638
simon_andres <simon.parra@cun.edu.co>
2026-09-10 07:33:09 -0500

    version 1 pagina

---
+++ script.js
@@ -0,0 +1,17 @@
+let temperatura = 10
+
+if (temperatura < 10) {
+    console.log('Hace mucho frio la temperatura es menor a 10 grados celsios')
+}
+else if (temperatura <= 20){
+    console.log('Hace frio la temperatura es menor a 20 grados centigrados')
+
+}   
+
+else if (temperatura <= 30){
+    console.log('Temperaruta agradable sobre el promedio')
+}
+
+else {
+    console.log('Hace mucho calor la temperatura es mayor a 30 grados centigrados')
+}
